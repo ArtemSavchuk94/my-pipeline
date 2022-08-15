1d6b818a3b561c82ca6cafa02d95de5e1cf7b18c
artemsavchuk94 <pro100artemqabin@gmail.com>
2022-08-15 18:49:45 +0300

    initial commit

--- bin/my-pipeline.ts
+++ bin/my-pipeline.ts
@@ -5,7 +5,7 @@ import { MyPipelineStack } from '../lib/my-pipeline-stack';
 const app = new cdk.App();
 new MyPipelineStack(app, 'MyPipelineStack', {
   env: {
-    account: '111111111111',
+    account: '181481668787',
     region: 'eu-west-1',
   }
 });
