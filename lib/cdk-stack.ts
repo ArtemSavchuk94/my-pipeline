02aa991bbefb01975e000d779d3652d9e1ef5277
artemsavchuk94 <pro100artemqabin@gmail.com>
2022-08-24 14:28:27 +0300

    stack b3 commit

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -7,6 +7,6 @@ export class CdkStack extends Stack {
         super(scope,id,props);
     const bucket = new Bucket(this,'my-bucket')
     const bucket2 = new Bucket(this,'my-bucket2')
-    const bucket3 = new Bucket(this,'my-bucket3')
+    
     }
 }
