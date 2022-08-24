02dd2b6a38331a6b0c93d9644a30f34ca2fa7aad
artemsavchuk94 <pro100artemqabin@gmail.com>
2022-08-24 14:47:10 +0300

    add destroy commit

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import { Bucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
@@ -6,10 +6,12 @@ export class CdkStack extends Stack {
     constructor(scope:Construct, id:string, props?: StackProps){
         super(scope,id,props);
     const bucket = new Bucket(this,'my-bucket',{
+        removalPolicy:RemovalPolicy.DESTROY,
         autoDeleteObjects:true,
     })
     const bucket2 = new Bucket(this,'my-bucket2',{
-      autoDeleteObjects:true,
+       removalPolicy:RemovalPolicy.DESTROY,
+       autoDeleteObjects:true,
     })
     
     }
